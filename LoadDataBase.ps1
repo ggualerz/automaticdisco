290623077ebadee24beb12b5861915aa4d9899f6
Your Name <you@example.com>
2025-01-25 05:47:28 -0800

    lololol

--- LoadDataBase.ps1
+++ LoadDataBase.ps1
@@ -16,9 +16,64 @@ if([string]::IsNullOrEmpty($Delim)){$Delim = ","}
 $CSV = Import-Csv -Delimiter $Delim -Path $CSVPath
 
 #Print Users
-Read-Host "Type Enter to print users"
-$CSV | Where-Object { $_.IsUser -eq "True" }
+Read-Host "Type Enter to load users"
+$Users = $CSV | Where-Object { $_.IsUser -eq "True" }
 
+# Itterate users
+foreach ($User in $Users) {
+    $SamAccountName = $User.SamAccountName
+    $UPN = $User.UserPrincipalName
+    $GivenName = $User.GivenName
+
+    # Check mandatoy fields
+    if (-not [string]::IsNullOrEmpty($SamAccountName) -and -not [string]::IsNullOrEmpty($UPN) -and -not [string]::IsNullOrEmpty($GivenName)) {
+
+        # Check if user already exist
+        $ExistingUser = Get-ADUser -Filter {SamAccountName -eq $SamAccountName} -ErrorAction SilentlyContinue
+
+        if (-not $ExistingUser) {
+            Write-Host "User creation : $SamAccountName $GivenName"
+
+            # Create new user
+            New-ADUser `
+                -SamAccountName $SamAccountName `
+                -UserPrincipalName $UPN `
+                -Name $User.GivenName `
+                -GivenName $User.GivenName `
+                -Surname $User.Surname `
+                -Enabled $true `
+                -AccountPassword (ConvertTo-SecureString "Pass42!" -AsPlainText -Force) `
+                -ChangePasswordAtLogon $true
+
+        } else {
+            Write-Host "User $SamAccountName already exist"
+        }
+    } else {
+        Write-Host "User with missing fields (login/name/...), ignored $($User.SamAccountName)"
+    }
+}
 #Print Group
-Read-Host "Type Enter to print groups"
-$CSV | Where-Object { $_.IsUser -eq "False" }
+Read-Host "Type Enter to load groups"
+$Groups = $CSV | Where-Object { $_.IsUser -eq "False" }
+
+# Iterate Groups
+foreach ($Group in $Groups) {
+    if ($Group.Name) { # Check mandatory fields
+        $GroupName = $Group.Name
+
+        # Check if group already exist
+        if (-not (Get-ADGroup -Filter { Name -eq $GroupName } -ErrorAction SilentlyContinue)) {
+            try {
+                # Créer le groupe
+                New-ADGroup -Name $GroupName -GroupScope Global -GroupCategory Security -Description "Group created with LoadDatabase.ps1"
+                Write-Output "Group '$GroupName' created."
+            } catch {
+                Write-Error "Error while creating '$GroupName': $_"
+            }
+        } else {
+            Write-Output " '$GroupName' already exist."
+        }
+    } else {
+        Write-Host "Missing field name in a group, cannot create the group"
+    }
+}
--- SaveDataBase.ps1
+++ SaveDataBase.ps1
@@ -4,27 +4,32 @@ $CSVPath = Read-Host "CSV Output path and name C:\Users\contoso\export.csv (defa
 #Get Delimiter
 $Delim = Read-Host "Choose the delimiter char (default is ',')"
 #Get Properties to fetch
-$Properties = Read-Host "Which properties to fetch, separate them with ',' (default is Name)"
+$Properties = Read-Host "Which properties to fetch, separate them with ',' (default is all)"
 
 
 
 #Apply default values if empty
 if([string]::IsNullOrEmpty($CSVPath)){$CSVPath = "$(Get-Location)\export.csv"}
 if([string]::IsNullOrEmpty($Delim)){$Delim = ","}
-if([string]::IsNullOrEmpty($Properties)){$Properties = "Name"}
+if([string]::IsNullOrEmpty($Properties)){$Properties = "*"}
 
 
 #Transform properties string into array of string
 $PropertiesArr = $Properties -split ",\s*"
 
 #Get all Users with the correct arguments
-$Users = Get-ADUser -Filter * -Properties $PropertiesArr 
+$Users = Get-ADUser -Filter * -Properties *
 #Add a boolean to know if its an user or group
 $Users | Add-Member -MemberType NoteProperty -Name 'IsUser' -Value $true -Force
 
 #Get all Groups with the correct arguments
-$Groups = Get-ADGroup -Filter * -Properties $PropertiesArr
+$Groups = Get-ADGroup -Filter * -Properties *
 #Add a boolean to know if its an user or group
 $Groups | Add-Member -MemberType NoteProperty -Name 'IsUser' -Value $false -Force
+if($Properties -ne "*")
+{
+    $PropertiesArr = @("IsUser") + $PropertiesArr
+}
 #Export All AD Users with arguments
-$Users + $Groups | Export-Csv -Path $CSVPath -Delimiter $Delim -NoTypeInformation
+$Merged = $Users + $Groups
+$Merged | Select-Object -Property $PropertiesArr | Export-Csv -Path $CSVPath -Delimiter $Delim -NoTypeInformation
